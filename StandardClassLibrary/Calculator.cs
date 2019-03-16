b50505ed41ef36e35ec73286f6f6af3f8f18d880
Duohong <duohongopen@gmail.com>
2019-03-16 19:12:33 -0400

    add AzureSamples

---
+++ StandardClassLibrary/Calculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace StandardClassLibrary
+{
+    public interface ICalculator
+    {
+        int Add(int a);
+    }
+
+    public class Calculator : ICalculator
+    {
+        private int mMemory;
+
+        public Calculator()
+            : this (0)
+        { }
+
+        public Calculator(int memory)
+        {
+            mMemory = memory;
+        }
+
+        public int Add(int a)
+        {
+            mMemory += a;
+            return mMemory;
+        }
+
+        public int Clear()
+        {
+            mMemory = 0;
+            return mMemory;
+        }
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using StandardClassLibrary;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var calculator = new Calculator();
+            calculator.Add(2);
+
+            return new string[] { "value1", "value2", calculator.Add(3).ToString() };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
